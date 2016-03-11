e3b7c3eea8b6ab95d6b1b1b7e0011321450ed267
Ryan Langseth <langseth@iteris.com>
2016-03-11 00:49:11 -0600

    Fixed up some of the todo items and ugly hack for the ignore list
    Moved idle list check into the Context and switched to a single return.
    Removed "bad_hack", replaced it with a default list containing None.

--- src/checkopenbgpd/checkopenbgpd.py
+++ src/checkopenbgpd/checkopenbgpd.py
@@ -78,12 +78,6 @@ class CheckBgpCtl(nagiosplugin.Resource):
         if state.isdigit():
             result = int(state)
 
-        #XXX: does not work as expected with the new Context
-        if state == 'Idle':
-            if self.idle_list is not None:
-                if session.Neighbor in self.idle_list:
-                    result = 0
-
         return state
 
     def probe(self):
@@ -100,19 +94,20 @@ class CheckBgpCtl(nagiosplugin.Resource):
 class BgpStatus(nagiosplugin.Context):
     """Context check of the BGP session"""
 
-    def __init__(self, name, warning, critical, fmt_metric=None,
+    def __init__(self, name, warning, critical, idle_list, fmt_metric=None,
                     result_cls=nagiosplugin.Result):
         self.warning = int(warning)
         self.critical = int(critical)
+        self.idle_list = idle_list
         self.name = name
         self.result_cls = result_cls
         self.fmt_metric = fmt_metric
 
 
     def evaluate(self, metric, resource):
+        state = nagiosplugin.state.Unknown
         if metric.value.isdigit():
             #XXX: All of this could/should be done with a ScalarContext
-            state = nagiosplugin.state.Unknown
             value = int(metric.value)
             if value < self.warning:
                 state = nagiosplugin.state.Warn
@@ -120,11 +115,14 @@ class BgpStatus(nagiosplugin.Context):
                 state = nagiosplugin.state.Critical
             elif value >= self.warning:
                 state = nagiosplugin.state.Ok
-
-            return self.result_cls(state,
-                "%s=%s" % (metric.name, metric.value), metric)
         else:
-            return self.result_cls(nagiosplugin.state.Critical,
+            state = nagiosplugin.state.Critical
+            if metric.value == 'Idle':
+                if self.idle_list is not None:
+                    if metric.name in self.idle_list:
+                        state = nagiosplugin.state.Ok
+
+        return self.result_cls(state,
                 "%s=%s" % (metric.name, metric.value), metric)
 
 
@@ -157,7 +155,7 @@ def parse_args():  # pragma: no cover
     argp.add_argument('-v', '--verbose', action='count', default=0,
                       help='increase output verbosity (use up to 3 times)')
     argp.add_argument('--idle-list', nargs='*', )
-    argp.add_argument('--ignore-list', default="asdf_really_bad_hack",nargs='*', )
+    argp.add_argument('--ignore-list', default=[None], nargs='*', )
     argp.add_argument('--socket', '-s', action='store', default=default_socket,
                       help="path to openbgpd socket (default: %(default)s)")
     argp.add_argument('--warning', '-w', type=int, default=0,
@@ -172,7 +170,8 @@ def main():  # pragma: no cover
 
     args = parse_args()
     check = nagiosplugin.Check(CheckBgpCtl(args.idle_list, args.ignore_list, args.socket),
-                               BgpStatus('bgpctl', args.warning, args.critical),
+                               BgpStatus('bgpctl', args.warning, args.critical,
+                               args.idle_list),
                                AuditSummary())
     check.main(args.verbose)
 
